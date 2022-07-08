159ec8d463942d3ceafd73d385273bd0add615aa
Justin Reock <jreock@gmail.com>
2022-07-08 18:27:32 -0400

    Update Cluster module code

--- cluster-autoscale-exit.js
+++ cluster-autoscale-exit.js
@@ -3,12 +3,24 @@ var http = require('http');
 
 if (cluster.isPrimary) { 
   var numWorkers = require('os').cpus().length;
+  var wids = [];
 
   console.log("Setting up " + numWorkers + " workers...");
 
   for (var i = 0; i < numWorkers; i++) { 
     cluster.fork(); 
   } 
+
+  for (wid in cluster.workers) {
+    wids.push(wid);
+  }
+
+  wids.forEach(function(wid) {
+    cluster.workers[wid].send({
+      type: 'shutdown',
+      from: 'primary'
+    });
+  });
 
   cluster.on('online', function(worker) {
     console.log('Worker ' + worker.process.pid + ' is online!');
@@ -17,8 +29,6 @@ if (cluster.isPrimary) {
   cluster.on('exit', function(worker,code,signal) {
     console.log('Worker ' + worker.process.pid + ' died with code: ' + code +
     ', and signal: ' + signal);
-    console.log('Starting a new worker');
-    cluster.fork();
   });
 
 } 
@@ -26,6 +36,12 @@ else {
   http.createServer(function(req, res)    { 
     res.writeHead(200); 
     res.end('process ' + process.pid + ' says hello!'); 
-  }).listen(8000); 
+  }).listen(8000);
+  process.on('message', function(message) {
+    if (message.type === 'shutdown') {
+      console.log('Shutting down worker ' + process.pid);
+      process.exit(0);
+    }
+  });
 }
 
