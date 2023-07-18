70de985b18dbeb81155c93eea6a0364f89d0b4f4
Justin Reock <jreock@gmail.com>
2023-07-18 17:25:01 -0600

    Clean up code and add comments

--- cluster-autoscale-exit.js
+++ cluster-autoscale-exit.js
@@ -1,48 +1,64 @@
-var cluster = require('cluster'); 
-var http = require('http'); 
+// NodeJS Deeper Dive
+// This code creates a multi-process HTTP server using Node.js' Cluster module. In this version, the master process creates 
+// a fixed number of worker processes, instead of basing it on the number of CPU cores. The master process also sends a shutdown 
+// message to each worker process. If a worker process receives a shutdown message, it gracefully shuts itself down.
 
+// Importing the necessary modules
+const cluster = require('cluster'); 
+const http = require('http'); 
+
+// Checking if the current process is the master
 if (cluster.isPrimary) { 
-  //var numWorkers = require('os').cpus().length;
-  var numWorkers = 4;
-  var wids = [];
 
-  console.log("Setting up " + numWorkers + " workers...");
+  // Setting the number of workers explicitly
+  const numWorkers = 4;
 
-  for (var i = 0; i < numWorkers; i++) { 
+  // Array to hold worker ids
+  let workerIDs = [];
+
+  console.log(`Setting up ${numWorkers} workers...`);
+
+  // Creating a new worker for each CPU core
+  for (let i = 0; i < numWorkers; i++) { 
     cluster.fork(); 
   } 
 
-  for (wid in cluster.workers) {
-    wids.push(wid);
+  // Populating the workerIDs array with worker ids
+  for (let workerID in cluster.workers) {
+     workerIDs.push(workerID);
   }
 
-  wids.forEach(function(wid) {
-    cluster.workers[wid].send({
-      type: 'shutdown',
-      from: 'primary'
-    });
+  // Sending a shutdown message to each worker
+  workerIDs.forEach(workerID => {
+     cluster.workers[workerID].send({
+        type: 'shutdown',
+        from: 'master'
+     });
   });
 
-  cluster.on('online', function(worker) {
-    console.log('Worker ' + worker.process.pid + ' is online!');
+  // Event handler for when a worker comes online
+  cluster.on('online', worker => {
+    console.log(`Worker ${worker.process.pid} is online!`);
   });
 
-  cluster.on('exit', function(worker,code,signal) {
-    console.log('Worker ' + worker.process.pid + ' died with code: ' + code +
-    ', and signal: ' + signal);
+  // Event handler for when a worker exits
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`Worker ${worker.process.pid} died with code: ${code}, and signal: ${signal}`);
   });
 
-} 
-else { 
-  http.createServer(function(req, res)    { 
+} else { 
+
+  // The worker processes create an HTTP server
+  http.createServer((req, res) => { 
     res.writeHead(200); 
-    res.end('process ' + process.pid + ' says hello!'); 
-  }).listen(8000);
-  process.on('message', function(message) {
+    res.end(`process ${process.pid} says hello!`); 
+  }).listen(8000); 
+
+  // Event handler for receiving a message from the master
+  process.on('message', message => {
     if (message.type === 'shutdown') {
-      console.log('Shutting down worker ' + process.pid);
+      console.log(`Shutting down worker ${process.pid}`);
       process.exit(0);
     }
   });
 }
-
